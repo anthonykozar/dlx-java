c132bf8ead390dc9460161e25a54ceca17d9264b
Anthony Kozar <akozar@users.sourceforge.net>
2023-04-06 09:03:14 -0400

    New examples of fitting L and T tetrominoes into a 4x4 grid.

---
+++ src/L-tetrominoes-4x4.py
@@ -0,0 +1,23 @@
+# Write an input file for DLXMain that
+# describes the problem of how to find all 
+# ways to tile a 4x4 grid with L tetrominoes.
+
+from polyominos import *
+
+fout = open('L-tetrominoes-4x4.txt', 'w')
+if fout:
+    # write the dimensions of the matrix
+    fout.write('16 0 48\n') # 16 cols, 48 rows
+    # use cell coords as the column headers
+    for row in range(4):
+        for col in range(4):
+            fout.write('(%d,%d) ' % (row, col))
+    fout.write('\n')
+    # write one row for each orientation and position of an L tetromino
+    b = tetrominos[1]
+    for side in range(2):
+        for rotation in range(4):
+            printallmatrixrows(b, 4, 4, out = fout)
+            b = polyrotate90(b)
+        b = polyreflectXY(b)
+    fout.close()
---
+++ src/T-tetrominoes-4x4.py
@@ -0,0 +1,21 @@
+# Write an input file for DLXMain that
+# describes the problem of how to find all 
+# ways to tile a 4x4 grid with T tetrominoes.
+
+from polyominos import *
+
+fout = open('T-tetrominoes-4x4.txt', 'w')
+if fout:
+    # write the dimensions of the matrix
+    fout.write('16 0 24\n') # 16 cols, 24 rows
+    # use cell coords as the column headers
+    for row in range(4):
+        for col in range(4):
+            fout.write('(%d,%d) ' % (row, col))
+    fout.write('\n')
+    # write one row for each orientation and position of a T tetromino
+    b = tetrominos[2]
+    for i in range(4):
+        printallmatrixrows(b, 4, 4, out = fout)
+        b = polyrotate90(b)
+    fout.close()
