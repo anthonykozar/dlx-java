eec2b7b48da37fd9a6d2bc4d163efbb60b33975f
Anthony Kozar <akozar@users.sourceforge.net>
2023-04-07 15:11:57 -0400

    Convert tabs to spaces.

--- src/one-pentomino-nxn.py
+++ src/one-pentomino-nxn.py
@@ -17,32 +17,32 @@ args = parser.parse_args()
 gridwidth = args.width
 gridheight = args.height
 if args.all:
-	names = "FILNPTUVWXYZ"
+    names = "FILNPTUVWXYZ"
 else:
-	names = args.pentominoes
+    names = args.pentominoes
 
 for name in names:
-	if args.stdout:
-		fout = sys.stdout
-	else:
-		filename = name + '-pentominoes-%dx%d.txt' % (gridwidth, gridheight)
-		fout = open(filename, 'w')
-	if fout:
-	    # write the dimensions of the matrix
-	    numcols = gridwidth * gridheight
-	    numrows = 10
-	    fout.write('%d 0 %d\n' % (numcols, numrows))
-	    # use cell coords as the column headers
-	    for row in range(gridheight):
-	        for col in range(gridwidth):
-	            fout.write('(%d,%d) ' % (row, col))
-	    fout.write('\n')
-	    # write one row for each orientation and position of an L tetromino
-	    b = pentominoes[name]
-	    for side in range(2):
-	        for rotation in range(4):
-	            printallmatrixrows(b, gridwidth, gridheight, out = fout)
-	            b = polyrotate90(b)
-	        b = polyreflectXY(b)
-	    if not args.stdout:
-	    	fout.close()
+    if args.stdout:
+        fout = sys.stdout
+    else:
+        filename = name + '-pentominoes-%dx%d.txt' % (gridwidth, gridheight)
+        fout = open(filename, 'w')
+    if fout:
+        # write the dimensions of the matrix
+        numcols = gridwidth * gridheight
+        numrows = 10
+        fout.write('%d 0 %d\n' % (numcols, numrows))
+        # use cell coords as the column headers
+        for row in range(gridheight):
+            for col in range(gridwidth):
+                fout.write('(%d,%d) ' % (row, col))
+        fout.write('\n')
+        # write one row for each orientation and position of an L tetromino
+        b = pentominoes[name]
+        for side in range(2):
+            for rotation in range(4):
+                printallmatrixrows(b, gridwidth, gridheight, out = fout)
+                b = polyrotate90(b)
+            b = polyreflectXY(b)
+        if not args.stdout:
+            fout.close()
