690b82caaee53943a87d2e095693e342653e55fd
Anthony Kozar <akozar@users.sourceforge.net>
2023-04-11 23:53:59 -0400

    Unfinished: Rewriting polyominoes.py as a class (Polyomino).

--- src/polyominoes.py
+++ src/polyominoes.py
@@ -1,37 +1,116 @@
 import sys
 
-def str2polyomino(polystr, cellch = 'x', minimizecoords = True):
-    a = []
-    row = 0
-    col = 0
-    lines = polystr.splitlines()
-    # skip initial blank lines
-    skip = 0
-    while lines[skip] == '': skip += 1
-    # scan lines for cellch
-    for ln in lines[skip:]:
+class Polyomino(object):
+    
+    def __init__(self, polystr, cellch = 'x', minimizecoords = True):
+        self.cells = []
+        row = 0
         col = 0
-        for c in ln:
-            if c == cellch:
-                a.append((col,row))
-            col += 1
-        row += 1
-    if minimizecoords:
-        # shift coordinates so that the minimum x and y values are both zero
+        lines = polystr.splitlines()
+        # skip initial blank lines
+        skip = 0
+        while lines[skip] == '': skip += 1
+        # scan lines for cellch
+        for ln in lines[skip:]:
+            col = 0
+            for c in ln:
+                if c == cellch:
+                    self.cells.append((col,row))
+                col += 1
+            row += 1
+        if minimizecoords:
+            # shift coordinates so that the minimum x and y values are both zero
+            col = lambda e: e[0]
+            row = lambda e: e[1]
+            minx = min(map(col, self.cells))
+            miny = min(map(row, self.cells))
+            if minx != 0 or miny != 0:
+                self.cells = [(x-minx, y-miny) for (x,y) in self.cells]
+
+    def __str__(self):
+        return str(self.cells)
+
+    def minmax(a, fieldfunc):
+        low = fieldfunc(a[0])
+        high = low
+        for e in a:
+            val = fieldfunc(e)
+            if val < low:
+                low = val
+            elif val > high:
+                high = val
+        return (low, high)
+
+    def polydims(self, minmax = minmax):
         col = lambda e: e[0]
         row = lambda e: e[1]
-        minx = min(map(col, a))
-        miny = min(map(row, a))
-        if minx != 0 or miny != 0:
-            a = [(x-minx, y-miny) for (x,y) in a]
-    return a
+        colmin, colmax = minmax(self.cells, col)
+        rowmin, rowmax = minmax(self.cells, row)
+        return (colmax - colmin + 1, rowmax - rowmin + 1)
 
-domino = str2polyomino('xx', 'x')
+    def polyshift(self, xoffset, yoffset):
+        return [(x+xoffset, y+yoffset) for (x,y) in self.cells]
+
+    # rotate p 90 degrees clockwise
+    def polyrotate90(self, minmax = minmax):
+        row = lambda e: e[1]
+        rowmin, rowmax = minmax(self.cells, row)
+        xoffset = rowmax + rowmin
+        return [(xoffset-y, x) for (x,y) in self.cells]
+
+    # reflect p in the XY diagonal (y=x)
+    def polyreflectXY(self):
+        return [(y,x) for (x,y) in self.cells]
+
+    def polyprint(self, gridwidth = None, gridheight = None, out = sys.stdout):
+        cols = max(map(lambda e: e[0], self.cells)) + 1
+        rows = max(map(lambda e: e[1], self.cells)) + 1
+        if not gridheight:
+            gridheight = rows
+        if not gridwidth:
+            gridwidth = cols
+        for y in xrange(gridheight):
+            for x in xrange(gridwidth):
+                if (x,y) in self.cells:
+                    out.write('x')
+                else:
+                    out.write('.')
+            out.write('\n')
+
+    def printallpositions(self, gridwidth, gridheight, polydims = polydims, polyprint = polyprint, polyshift = polyshift):
+        cols, rows = self.polydims()
+        for yoffset in range(gridheight-rows+1):
+            for xoffset in range(gridwidth-cols+1):
+                self.polyshift(xoffset, yoffset).polyprint(gridwidth, gridheight)
+                print ' '
+
+    def polyprintmatrixrow(self, gridwidth, gridheight, precols = [], postcols = [], out = sys.stdout):
+        if precols != []:
+            prestr = ' '.join(map(str, precols)) + ' '
+            out.write(prestr)
+        for y in xrange(gridheight):
+            for x in xrange(gridwidth):
+                if (x,y) in self.cells:
+                    out.write('1 ')
+                else:
+                    out.write('0 ')
+        if postcols != []:
+            poststr = ' '.join(map(str, postcols))
+            out.write(poststr)
+        out.write('\n')
+
+    def printallmatrixrows(self, gridwidth, gridheight, precols = [], postcols = [], out = sys.stdout, polydims = polydims, polyshift = polyshift, polyprintmatrixrow = polyprintmatrixrow):
+        cols, rows = self.polydims()
+        for yoffset in range(gridheight-rows+1):
+            for xoffset in range(gridwidth-cols+1):
+                self.polyshift(xoffset, yoffset).polyprintmatrixrow(gridwidth, gridheight, precols, postcols, out)
+
+domino = Polyomino('xx', 'x')
 v = '''
 x
 x
 '''
-vdomino = str2polyomino(v, 'x')
+vdomino = Polyomino(v, 'x')
 trominostr = '''
 000
 11.
@@ -90,81 +169,7 @@ NN.
 .N.
 '''
 
-trominoes = [str2polyomino(trominostr, str(d)) for d in range(2)]
-tetrominoes = [str2polyomino(tetrominostr, str(d)) for d in range(5)]
-pentominoes = {c:str2polyomino(pentominostr, c) for c in "ILYPUVTFZXWN"}
+trominoes = [Polyomino(trominostr, str(d)) for d in range(2)]
+tetrominoes = [Polyomino(tetrominostr, str(d)) for d in range(5)]
+pentominoes = {c:Polyomino(pentominostr, c) for c in "ILYPUVTFZXWN"}
 
-def minmax(a, fieldfunc):
-    low = fieldfunc(a[0])
-    high = low
-    for e in a:
-        val = fieldfunc(e)
-        if val < low:
-            low = val
-        elif val > high:
-            high = val
-    return (low, high)
-
-def polydims(p, minmax = minmax):
-    col = lambda e: e[0]
-    row = lambda e: e[1]
-    colmin, colmax = minmax(p, col)
-    rowmin, rowmax = minmax(p, row)
-    return (colmax - colmin + 1, rowmax - rowmin + 1)
-
-def polyshift(p, xoffset, yoffset):
-    return [(x+xoffset, y+yoffset) for (x,y) in p]
-
-# rotate p 90 degrees clockwise
-def polyrotate90(p, minmax = minmax):
-    row = lambda e: e[1]
-    rowmin, rowmax = minmax(p, row)
-    xoffset = rowmax + rowmin
-    return [(xoffset-y, x) for (x,y) in p]
-
-# reflect p in the XY diagonal (y=x)
-def polyreflectXY(p):
-    return [(y,x) for (x,y) in p]
-
-def polyprint(p, gridwidth = None, gridheight = None, out = sys.stdout, polydims = polydims):
-    cols = max(map(lambda e: e[0], p)) + 1
-    rows = max(map(lambda e: e[1], p)) + 1
-    if not gridheight:
-        gridheight = rows
-    if not gridwidth:
-        gridwidth = cols
-    for y in xrange(gridheight):
-        for x in xrange(gridwidth):
-            if (x,y) in p:
-                out.write('x')
-            else:
-                out.write('.')
-        out.write('\n')
-
-def printallpositions(p, gridwidth, gridheight, polydims = polydims, polyprint = polyprint, polyshift = polyshift):
-    cols, rows = polydims(p)
-    for yoffset in range(gridheight-rows+1):
-        for xoffset in range(gridwidth-cols+1):
-            polyprint(polyshift(p, xoffset, yoffset), gridwidth, gridheight)
-            print ' '
-
-def polyprintmatrixrow(p, gridwidth, gridheight, precols = [], postcols = [], out = sys.stdout):
-    if precols != []:
-        prestr = ' '.join(map(str, precols)) + ' '
-        out.write(prestr)
-    for y in xrange(gridheight):
-        for x in xrange(gridwidth):
-            if (x,y) in p:
-                out.write('1 ')
-            else:
-                out.write('0 ')
-    if postcols != []:
-        poststr = ' '.join(map(str, postcols))
-        out.write(poststr)
-    out.write('\n')
-
-def printallmatrixrows(p, gridwidth, gridheight, precols = [], postcols = [], out = sys.stdout, polydims = polydims, polyshift = polyshift, polyprintmatrixrow = polyprintmatrixrow):
-    cols, rows = polydims(p)
-    for yoffset in range(gridheight-rows+1):
-        for xoffset in range(gridwidth-cols+1):
-            polyprintmatrixrow(polyshift(p, xoffset, yoffset), gridwidth, gridheight, precols, postcols, out)
